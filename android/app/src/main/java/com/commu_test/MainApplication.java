4892aed4d985e3cf9fec8827cc37b20a5ad9c5b7
lsb918 <lsb918@uos.ac.kr>
2019-08-30 01:01:39 +0900

    updated to rn 0.60.5

--- android/app/src/main/java/com/commu_test/MainApplication.java
+++ android/app/src/main/java/com/commu_test/MainApplication.java
@@ -1,6 +1,11 @@
 package com.commu_test;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -12,7 +17,7 @@ import com.melihyarikkaya.rnserialport.RNSerialportPackage;
 import com.sensors.RNSensorsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -28,11 +33,13 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSerialportPackage(),
-            new RNSensorsPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new RNSerialportPackage());
+      packages.add(new RNSensorsPackage());
+      return packages;
     }
 
     @Override
